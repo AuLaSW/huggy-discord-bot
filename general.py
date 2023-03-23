28b5a31974fba029f17b7c7b7412984e8ae875a3
Austin <austin.swanlaw@gmail.com>
2023-03-23 12:54:12 -0500

    Add visual hug options.
    This adds options for the hug text and adds visuals (.mp4 files) that
    are sent with the hugs.
    
    Work needs to be done to send gifs instead of mp4 files, but it works
    for now.

--- general.py
+++ general.py
@@ -7,14 +7,46 @@ This module holds general commands for attaching to a discord bot.
 Every command module has to have a setup(bot) method for attaching
 the commands to the bot.
 """
+import random
+from pathlib import Path
 from discord.ext import commands
+import discord
+
+
 
 @commands.command()
-async def hello(ctx):
+async def hug(ctx, *mentions):
     channel = ctx.message.channel
-    await channel.send("Hello!")
+    author = ctx.message.author
+    
+    if len(mentions) == 0:
+        await channel.send(f"<@{author.id}> I need someone to hug!\nPlease!\nTell me who to hug!\n*shaking* M-my whole existence is hugging!\nWHO DO I HUG?!?!")
+        return
+
+    mention = " ".join(mentions)
+    
+    hugs_text = [
+        "I bet you needed that today!",
+        "Aww, come on in!",
+        "*licks your ear*",
+        "I don't know what you're wearing, but it smells great!",
+        "It's okay, I'm here for you.", 
+        "I hope your day gets better."
+    ]
+    
+    hug_text = random.choice(hugs_text)
+    
+    with random.choice(hugs_visual) as hug_visual:
+        file = discord.File(fp=hug_visual)
+        await channel.send(f"<@{author.id}> gave {mention} a hug! {hug_text}", file=file)
 
 
 def setup(bot):
     """Attaches commands to the incoming bot."""
-    bot.add_command(hello)
+    print("Adding hugs...")
+    print("Generating hug visuals...")
+    global hugs_visual
+    hugs_visual = list(Path('./content/').glob('*.mp4'))
+    for path in hugs_visual:
+        print(path)
+    bot.add_command(hug)
